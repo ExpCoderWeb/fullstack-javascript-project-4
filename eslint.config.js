518826ec60aaa6815bdabb36dc8da56f73dee8c1
ExpCoderWeb <shagaev_2011@mail.ru>
2025-04-10 23:05:21 +0400

    modify eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -2,10 +2,12 @@ import path from 'path';
 import { fileURLToPath } from 'url';
 import { FlatCompat } from '@eslint/eslintrc';
 import pluginJs from '@eslint/js';
+import importPlugin from 'eslint-plugin-import';
 
-// mimic CommonJS variables -- not needed if using CommonJS
+// mimic 2 CommonJS variables -- not needed if using CommonJS
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+
 const compat = new FlatCompat({
   baseDirectory: __dirname,
   recommendedConfig: pluginJs.configs.recommended,
@@ -19,6 +21,10 @@ export default [
         ecmaVersion: 'latest',
         sourceType: 'module',
       },
+    },
+    plugins: { import: importPlugin },
+    rules: {
+      ...importPlugin.configs.recommended.rules,
     },
   },
   {
